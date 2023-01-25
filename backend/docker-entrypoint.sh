dc555d7ce205a01c9fbbe8b2fd19eef9344c5b0d
Ordigital <biuro@ordigital.pl>
2023-01-25 18:35:30 +0100

    Working version!

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 python manage.py migrate --no-input
-python manage.py createsuperuser
+#python manage.py createsuperuser
 exec gunicorn backend.wsgi:application --bind 0.0.0.0:7999 --workers 3
